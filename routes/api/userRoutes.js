c69166f3fc948589f7923398430bdae7143fc9cc
Daniel R <110840284+Daniel-robe@users.noreply.github.com>
2023-02-15 18:04:18 -0600

    Fixed issues

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -7,3 +7,5 @@ router.route('/').get(getUsers).post(createUser);
 router.route('/:userId').get(getUser).put(updateUser).delete(deleteUser);
 
 router.route('/:userId/friends/:friendsId').post(addFriend).delete(removeFriend)
+
+module.exports = router;
